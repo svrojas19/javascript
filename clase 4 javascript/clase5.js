773fe6bb45b8a5bdf30c75eea2656821ac3a78c9
svrojas19 <sofiav1927@gmail.com>
2022-07-21 00:19:20 -0500

    clase javascript

---
+++ clase 4 javascript/clase5.js
@@ -0,0 +1,29 @@
+let frutas = [ 'banano' ,'manzana' ,'fresa','mango','piña'];
+
+let numeros = [ 12, 13, 14, 15, 16, 17, 18];
+
+let cifrasTennis = [[1245,6785] , ['wimbledon', 'roland garros', 'us open']];
+
+
+console.log(frutas[1])
+
+
+
+const estudiantes = {
+    nombre: 'WWCODE',
+    edad: 25, 
+}
+ 
+
+const auto = {
+    modelo: 2023, 
+    marca: 'Mercedes',
+    "costo-beneficio": true, 
+
+}
+
+/*console.log(), 
+
+console.log('For of'); 
+for (const carro of carros),
+*/
